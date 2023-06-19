74413f2e96684faceb192bf8f24ef83d898395d6
Sriram Radhakrishna <sriram.radhakrishna42@gmail.com>
2023-06-20 03:47:22 +0530

    car files imported, multi robot launch problems need fixing
    changes since last commit :
    - new committing standard
    - world file finished
    - dofbot with male injector completed and placed
    - joint trajectory controller implemented
    
    near term features pending (deadline - 16th June, 2023) :
    - car urdf modified with female component and visual odometry pattern
    - car controller
    - demo scripts for static refuelling with cvbridge
    - new car urdf with arm & male connector
    - adjust inertia values for dofbot components
    - testing to collect data for static refuelling (write nodes)
        - time for refuelling
        - payload capacity
        - end effector details
        - arm velocity
        - edge detections
        - docking and undocking time
        - alignment accuracy while docking (x, y, z)
    
    long term features pending :
    - demo scripts for dynamic refuelling
    - patent & publication writing
    - multidimensional synchronization for dynamic case (tbd)
    - in-space arm docking (tbd)
    
    current commit policy - commit to main

---
+++ src/armjtc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+import sys
+
+def perform_trajectory():
+    rospy.init_node('arm_trajectory_publisher')
+    contoller_topic='arm/arm_controller/command'
+    trajectory_publihser = rospy.Publisher(contoller_topic,JointTrajectory, queue_size=10)
+    argv = sys.argv[1:]                         
+    arm_joints = ['joint1','joint2','joint3','joint4', 'joint5']
+    goal_positions = [ float(argv[0]) , float(argv[1]) , float(argv[2]) ,float(argv[3])  ]
+ 
+    rospy.loginfo("Goal Position set")
+    rospy.sleep(1)
+
+
+    trajectory_msg = JointTrajectory()
+    trajectory_msg.joint_names = arm_joints
+    trajectory_msg.points.append(JointTrajectoryPoint())
+    trajectory_msg.points[0].positions = goal_positions
+    trajectory_msg.points[0].velocities = [0.0 for i in arm_joints]
+    trajectory_msg.points[0].accelerations = [0.0 for i in arm_joints]
+    trajectory_msg.points[0].time_from_start = rospy.Duration(3)
+    rospy.sleep(1)
+    trajectory_publihser.publish(trajectory_msg)
+
+
+if __name__ == '__main__':
+    perform_trajectory()
